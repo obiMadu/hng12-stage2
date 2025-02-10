85d642e60dac0da27efc0975f9092b1fd0e78b2b
Obi.M <mail@obimadu.pro>
2025-02-10 17:54:59 +0100

    chore: Update Docker Compose commands in deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,8 @@ cd ~/hng12-stage2
 git pull origin main
 
 # Build and restart Docker container
-docker-compose down
-docker-compose up -d --build
+docker compose down
+docker compose up -d --build
 
 # Reload Nginx
 sudo systemctl reload nginx
